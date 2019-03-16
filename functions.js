ce05830dfb2f29c21a9c16cdaa0d6edc685dfdfa
rossjohnwilliamson <47782319+rossjohnwilliamson@users.noreply.github.com>
2019-03-16 22:35:14 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+
+
+
+
 const matchFirstLetterInName = (checking,searching) => {
 
 const regex = new RegExp("^"+searching, "i");
@@ -5,12 +9,34 @@ return regex.test(checking);
 
 }
 
-const matchFullName = (checking,searching) => {
 
-const regex = new RegExp(searching, "i");
-return regex.test(checking);
+
+const sliceMemes = (memes) => {
+
+const indexOfSpace = memes.indexOf(" ");
+const slice = memes.slice(indexOfSpace+1);
+return slice;
+
 
 }
+
+
+const slice = (memes) => {
+
+const indexOfSpace = memes.indexOf(" ");
+const firstLetter = memes.charAt(0);
+const slice = memes.slice(indexOfSpace+1);
+const newSlice = slice.slice(1);
+return firstLetter.toUpperCase() + newSlice;
+
+
+}
+
+
+
+
+
+
 
 
 
