2716b23736542f5eda1fbd4d75d222f572314629
rossjohnwilliamson <47782319+rossjohnwilliamson@users.noreply.github.com>
2019-03-17 09:07:48 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,53 @@
+
+
+
 const name = (checking,searching) => {
 
 const regex = new RegExp(searching, "i","g");
 return regex.test(checking);
 
 }
+
+
+const sortAlph = (a,b) =>{
+
+
+  const genreA = a.name.toLowerCase();
+  const genreB = b.name.toLowerCase();
+
+  let comparison = 0;
+  if (genreA > genreB) {
+    comparison = 1;
+  } 
+
+  else if (genreA < genreB) {
+    comparison = -1;
+  }
+  return comparison;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
