12111f0b49e340b6f9ea7632e3eb0f8b34794bae
rossjohnwilliamson <47782319+rossjohnwilliamson@users.noreply.github.com>
2019-03-17 08:22:39 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,61 +1,7 @@
-
-  
-
-
-const matchFirstLetterInName = (checking,searching) => {
-
-const regex = new RegExp("^"+searching, "i");
-return regex.test(checking);
-
-}
-
-
-
-const sliceMemes = (memes) => {
-
-const indexOfSpace = memes.indexOf(" ");
-const slice = memes.slice(indexOfSpace+1);
-return slice;
-
-
-}
-
-
-const lastName = (checking,searching) => {
+const name = (checking,searching) => {
 
 const regex = new RegExp(searching, "i","g");
 return regex.test(checking);
 
 }
-
-
-
-const slice = (memes) => {
-
-const indexOfSpace = memes.indexOf(" ");
-const firstLetter = memes.charAt(0);
-const slice = memes.slice(indexOfSpace+1);
-const newSlice = slice.slice(1);
-return firstLetter.toUpperCase() + newSlice;
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
